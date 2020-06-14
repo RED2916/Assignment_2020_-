da33a30747f3ba7d2f0fadf2db6b126da5c64f07
RED2916 <56479635+RED2916@users.noreply.github.com>
2020-06-14 15:38:23 +0800

    Create ChooseGame.h

---
+++ head/ChooseGame.h
@@ -0,0 +1,16 @@
+#ifndef CHOOSEGAME_H
+#define CHOOSEGAME_H
+
+#include <atlstr.h>
+
+//全局变量区，会被游戏区使用
+
+/*using namespace std;//别放在头文件，会出错！
+
+CString input_game_name;
+CString input_game_author;
+int col_num,row_num;*/
+
+void Choose_Game_Face();
+
+#endif
